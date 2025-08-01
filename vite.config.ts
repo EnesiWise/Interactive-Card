d8d98a0a57dcd8dc7f46f5f391169904ebb8e4d3
Enesi Wise <enesiwise@gmail.com>
2025-08-01 21:53:49 +0100

    Add 404.html for my custom NotFound page

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Interactive-Card/" : "/",
+  base: mode === "production" ? "/Interactive-Card/" : "/Interactive-Card/",
 
   server: {
     host: "::",
